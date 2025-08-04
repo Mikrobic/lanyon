5ca4db137624db3641d5d40ee3c7608e00240b17
Your Name <you@example.com>
2025-08-04 22:38:47 +0300

    Форматирование файлов

--- public/js/script.js
+++ public/js/script.js
@@ -12,4 +12,19 @@
 
     checkbox.checked = false;
   }, false);
+
+
+ function checkMobile() {
+  if (window.innerWidth <= 768) {
+    document.body.classList.add('mobile-view');
+    // Закрываем сайдбар по умолчанию на мобильных
+    sidebarCheckbox.checked = false;
+  } else {
+    document.body.classList.remove('mobile-view');
+  }
+}
+
+// Проверяем при загрузке и при изменении размера окна
+window.addEventListener('load', checkMobile);
+window.addEventListener('resize', checkMobile);
 })(document);
