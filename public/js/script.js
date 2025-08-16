7e9f3212e4c5f5cc41e83e330309bc6c080c2171
Sergey Timakin <123402290+Mikrobic@users.noreply.github.com>
2025-08-16 14:07:14 +0300

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,10 @@
 (function(document) {
+  // Элементы для управления сайдбаром
   var toggle = document.querySelector('.sidebar-toggle');
   var sidebar = document.querySelector('#sidebar');
   var checkbox = document.querySelector('#sidebar-checkbox');
 
+  // Закрытие сайдбара при клике вне его
   document.addEventListener('click', function(e) {
     var target = e.target;
 
@@ -12,4 +14,14 @@
 
     checkbox.checked = false;
   }, false);
+
+  // Управление подменю
+  document.querySelectorAll('.sidebar-nav-toggle').forEach(function(toggle) {
+    toggle.addEventListener('click', function(e) {
+      e.preventDefault();
+      var submenu = this.nextElementSibling;
+      submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+      this.classList.toggle('open');
+    });
+  });
 })(document);
